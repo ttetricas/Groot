92e3b3614cfe661576283a410685da231e6c2cc3
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-06-13 22:13:31 -0300

    Groot Chatbot

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿ Console.WriteLine("--- Groot Chatbot ---\n");
+
+ while (true)
+{
+    Console.Write("Pergunta:");
+    string pergunta = Console.ReadLine()!.Trim();
+
+    if (pergunta.ToUpper() == "TCHAU")
+    {
+        Console.WriteLine("                          Resposta: Eu sou Groot!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("                          Resposta: Eu sou Groot.");
+    }
+}
